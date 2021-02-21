8d54925a2b2f24edc85106797f0d5b9eb1d7633d
Sebastian Tkaczyk <sebastian.navycoat@gmail.com>
2021-02-21 13:27:18 +0100

    Create GraphResources
    I need some kind of storage for resources and relations between them. I added this simple graphlike storage for use cases described in the task.

---
+++ src/GraphResources.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace Cleeng\Entitlements;
+
+
+class GraphResources
+{
+    private array $array;
+
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    /**
+     * TODO: Should I pass id?
+     * I can imagine that Resources are managed in other module (CRUD)
+     * In this case, I rather care about the access relationship itself.
+     * I think that giving availability to make 1:1 id of resource in this module and in "catalog" is ok.
+     */
+    public function addResource($id, array $children = [], array $parents = [])
+    {
+        foreach ($parents as $parent) {
+            $this->array[$parent][] = $id;
+        }
+        $this->array[$id] = $children;
+    }
+
+    public function isResourceAvailableToReach($resourceId, array $entitlements): bool
+    {
+        if ($this->isResourceInEntitlements($resourceId, $entitlements)) {
+            return true;
+        }
+
+        return $this->findInResources($entitlements, $resourceId);
+    }
+
+    private function findInResources(array $entitlements, $resourceId): bool
+    {
+        foreach ($entitlements as $entitlement) {
+            if (!array_key_exists($entitlement, $this->array)) {
+                continue;
+            }
+
+            $relatedResources = $this->array[$entitlement];
+            if ($relatedResources) {
+                foreach ($relatedResources as $relatedResourceID) {
+                    if ($resourceId === $relatedResourceID) {
+                        return true;
+                    }
+
+                    if ($this->findInResources($relatedResources, $resourceId)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $resourceId
+     * @param array $entitlements
+     * @return bool
+     */
+    private function isResourceInEntitlements($resourceId, array $entitlements): bool
+    {
+        return in_array($resourceId, $entitlements, true);
+    }
+
+}
---
+++ tests/GraphResourcesTest.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace Tests;
+
+use Cleeng\Entitlements\GraphResources;
+use PHPUnit\Framework\TestCase;
+
+class GraphResourcesTest extends TestCase
+{
+    /**
+     * @dataProvider resourceProvider
+     */
+    public function testResourcesAreCorrectlyRelated(
+        GraphResources $resources,
+        array $entitlements,
+        array $shouldHaveAccessTo,
+        array $shouldNotHaveAccessTo
+    ) {
+        foreach ($shouldHaveAccessTo as $resourceId) {
+            self::assertTrue(
+                $resources->isResourceAvailableToReach($resourceId, $entitlements),
+                sprintf('Failed to assert that resource "%s" is related to entitlements provided in test.', $resourceId)
+            );
+        }
+
+        foreach ($shouldNotHaveAccessTo as $resourceId) {
+            self::assertFalse(
+                $resources->isResourceAvailableToReach($resourceId, $entitlements),
+                sprintf(
+                    'Failed to assert that resource "%s" is not related to entitlements provided in test.',
+                    $resourceId
+                )
+            );
+        }
+    }
+
+    public function resourceProvider(): array
+    {
+        return [
+            [
+                'resources' => new GraphResources([1 => [2, 3, 4], 2 => [5, 6], 5 => [7, 8]]),
+                'entitlements' => [2],
+                'shouldHaveAccessTo' => [2, 5, 6, 7, 8],
+                'shouldNotHaveAccessTo' => [3, 4],
+            ],
+            [
+                'resources' => $this->createResourceUsingAddResource(),
+                'entitlements' => [2, 5],
+                'shouldHaveAccessTo' => [2, 5, 6, 7, 8],
+                'shouldNotHaveAccessTo' => [3, 4],
+            ],
+        ];
+    }
+
+    private function createResourceUsingAddResource()
+    {
+        $resource = new GraphResources([]);
+        $resource->addResource(1, [2, 3, 4]);
+        $resource->addResource(5, [7, 8], [2]);
+        $resource->addResource(6, [], [2]);
+
+        return $resource;
+    }
+
+}
